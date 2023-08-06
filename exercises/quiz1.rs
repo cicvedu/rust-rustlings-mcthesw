c48c4482ccd388950ecef8a98ab0210f6ad411fd
Sworld <t.sworld@qq.com>
2023-08-06 12:08:40 +0800

    first day

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(nums: i32) -> i32 {
+    if (nums <= 40) {
+        nums * 2
+    } else {
+        nums
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
